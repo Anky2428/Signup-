bf17fb993d81d9b96de2052104d62f680f9e46b8
Anky2428 <80748894+Anky2428@users.noreply.github.com>
2022-04-29 17:39:53 +0530

    Add files via upload

---
+++ fb[1].php
@@ -0,0 +1,48 @@
+<html> 
+<head>
+<title>Signup</title>
+
+</head>
+
+<body  style="background-color:lightblue">
+
+<center>
+	<!--<img src="facebook2.jpg">-->
+<form  method="post">
+
+
+
+<!--	<label >Email or Phone</label>
+
+	<input type="text"  name="email" id="email">
+	<label>Password</label>
+	<input type="password" id="password"><label>Forgotten account?</label><br>
+	<input type="submit" value="LogIn" action="formdata.php" >
+	<h1><b>Create an account</b></h1>
+<h2><b>It's free and always will be.</b></h2>-->
+
+	<br><input type="text" name="firstname" placeholder="firstname" id="firstname"   >   
+	    <input type="text" name="surname" placeholder="surname" id="surname" ><br>
+
+	<br> <input type="text" name="E-mail/phone" placeholder="E-mail/phone" id="E-mail/Phone"><br>
+	<br> <input type="password" name="password" placeholder="password" id="password" required pattern="[A-Za-z][!@#$%^&)}{][*(][0-9]{20}"><br>
+	<br><b>Birth Day:-</b><br>
+	<input type="number" name="Date" placeholder="Date" >
+    <input type="number" name="Month" placeholder="Month">
+	<input type="number" name="Year" placeholder="Year"><br>
+Gender
+	<input type="radio" name="gender" value="2">
+<label class="_58mt" for="u_0_a">Male</label>
+<input type="radio" name="gender" value="2">
+ <label class="58mt" for="u_0_a">Female</label><br><br><br>
+
+	  By clicking Sign Up, you agree to our Terms,<br>
+	 Data Policy and Cookie Policy. You may receive SMS<br>
+	  notifications from us and can opt out at any time.<br>
+
+
+	<br><input type="submit" value="LogIn" >
+</form>
+</center>	
+</body>
+</html>
